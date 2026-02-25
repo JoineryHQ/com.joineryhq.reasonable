d3a407d19c548cc2a548bcaab31e01a586ca3f52
Allen Shaw <allen@JoineryHQ.com>
2026-02-25 14:37:05 -0600

    Alert user on Settings form if available alterations have changed (and prompt to clear caches).

--- reasonable.php
+++ reasonable.php
@@ -61,5 +61,38 @@ function reasonable_civicrm_postProcess(string $formName, \CRM_Core_Form &$form)
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_buildForm
  */
 function reasonable_civicrm_buildForm(string $formName, \CRM_Core_Form &$form): void {
+  // Call any buildForm hooks in enabled alterations.
   CRM_Reasonable_Util::hook('buildForm', $formName, $form);
+
+  // Take our own specific action for the Settings form.
+  if ($formName == 'CRM_Admin_Form_Generic' && $form->getSettingPageFilter() == 'reasonable') {
+    $ext = CRM_Extension_Info::loadFromFile(E::path('info.xml'));
+    $alterations = CRM_Reasonable_Util::getAlterationClasses();
+
+    // Alert user to clear caches if new alterations have been made available in latest code.
+    foreach ($alterations as $alteration) {
+      $settingName = 'reasonable_alteration_' . $alteration;
+      if (!array_key_exists($settingName, $form->_elementIndex)) {
+        CRM_Core_Session::setStatus(E::ts('This version of "%1" contains some alterations not available on this screen. Please <a href="%2">cleanup CiviCRM caches</a> to see all available alterations.', [
+          '1' => $ext->label,
+          '2' => CRM_Utils_System::url('civicrm/admin/setting/updateConfigBackend', 'reset=1'),
+        ]));
+        break;
+      }
+    }
+
+    // Alert user to clear caches if OLD alterations have been made UNavailable in latest code.
+    foreach ($form->_elementIndex as $elementName => $elementIndexValue) {
+      if (strpos($elementName, 'reasonable_alteration_') === 0) {
+        $alterationName = preg_replace('/^reasonable_alteration_/', '', $elementName);
+        if (!in_array($alterationName, $alterations)) {
+          CRM_Core_Session::setStatus(E::ts('This screen offers alterations which have been removed from "%1". Please <a href="%2">cleanup CiviCRM caches</a> to update available alterations.', [
+            '1' => $ext->label,
+            '2' => CRM_Utils_System::url('civicrm/admin/setting/updateConfigBackend', 'reset=1'),
+          ]));
+          break;
+        }
+      }
+    }
+  }
 }
